9a11a122844fd837eb70a9d168c332db099f5662
Tyler van der Hoeven <hi@tylervdh.com>
2014-05-19 14:33:03 -0400

    updates to layout, bringing into Ionic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,5 @@
-angular.module('starter.controllers', [])
+angular.module('controllers', [])
 
-.controller('AppCtrl', function($scope) {
-  $scope.stopResumeTimer = function() {  
-    if (this.innerHTML === 'Start') {
-      document.getElementsByTagName('timer')[0].start();
-      this.innerHTML = 'Stop';
-    }
-    else if (this.innerHTML === 'Stop') {
-      document.getElementsByTagName('timer')[0].stop();
-      this.innerHTML = 'Resume';
-    }
-    else {
-      document.getElementsByTagName('timer')[0].resume();
-      this.innerHTML = 'Stop';
-    }  
-  }
+.controller('AppCtrl', function() {
+  
 });
