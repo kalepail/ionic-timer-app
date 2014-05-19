ff2228499a5645f40204a0a22e2552c8807080ba
Tyler van der Hoeven <hi@tylervdh.com>
2014-05-19 13:35:10 -0400

    Functioning timer

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'timer'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function() {
-  timer();
+.controller('AppCtrl', function($scope) {
+  $scope.stopResumeTimer = function() {  
+    if (this.innerHTML === 'Start') {
+      document.getElementsByTagName('timer')[0].start();
+      this.innerHTML = 'Stop';
+    }
+    else if (this.innerHTML === 'Stop') {
+      document.getElementsByTagName('timer')[0].stop();
+      this.innerHTML = 'Resume';
+    }
+    else {
+      document.getElementsByTagName('timer')[0].resume();
+      this.innerHTML = 'Stop';
+    }  
+  }
 });
