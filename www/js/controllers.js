75fa88f359baaf120a421966c7e9e377f8196480
Tyler van der Hoeven <hi@tylervdh.com>
2014-05-20 17:35:26 -0400

    more fancies

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,4 +12,24 @@ angular.module('controllers', [])
 
 .controller('TimerCtrl', function($scope, $stateParams) {
 	$scope.result = parseFloat($stateParams.goal);
+	$scope.class = "stop";
+  
+  $scope.timerToggle = function(sectionId, btn) {  
+    var items = document.getElementById(sectionId).getElementsByTagName('timer');
+    
+    if (btn.innerHTML === 'Stop') {
+      for (var i=0; i < items.length; i++) {
+        items[i].stop();
+        btn.innerHTML = 'Resume';
+        $scope.class = "start";
+      }
+    }
+    else {
+      for (var i=0; i < items.length; i++) {
+        items[i].resume();
+        btn.innerHTML = 'Stop';
+        $scope.class = "stop";
+      }
+    }
+  };
 })
