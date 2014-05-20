99c04558b624ec3a85e7bead0732e66be70f8196
Tyler van der Hoeven <hi@tylervdh.com>
2014-05-20 17:59:00 -0400

    fine point changes

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ angular.module('navy-timer', ['ionic', 'timer', 'controllers'])
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,35 @@
 angular.module('controllers', [])
 
+// Homepage Set-A-Goal Controller
 .controller('GoalCtrl', function($scope, $ionicSideMenuDelegate, $ionicScrollDelegate) {
 	$scope.submit = function() {
 	  if ($scope.text) {
 	    query = this.text;
-	    $scope.text = '';
 	    window.location.href = '#/timer?goal='+query+'';
 	  }
 	};
 })
 
+// Timer Controller
 .controller('TimerCtrl', function($scope, $stateParams) {
 	$scope.result = parseFloat($stateParams.goal);
-	$scope.class = "stop";
+	$scope.class = "pause";
   
   $scope.timerToggle = function(sectionId, btn) {  
     var items = document.getElementById(sectionId).getElementsByTagName('timer');
     
-    if (btn.innerHTML === 'Stop') {
+    if (btn.innerHTML === 'Pause') {
       for (var i=0; i < items.length; i++) {
         items[i].stop();
         btn.innerHTML = 'Resume';
-        $scope.class = "start";
+        $scope.class = "resume";
       }
     }
     else {
       for (var i=0; i < items.length; i++) {
         items[i].resume();
-        btn.innerHTML = 'Stop';
-        $scope.class = "stop";
+        btn.innerHTML = 'Pause';
+        $scope.class = "pause";
       }
     }
   };
